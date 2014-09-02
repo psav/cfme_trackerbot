d88a2364e28ce690baaad948ece7ca4f069cfe0a
Sean Myers <sean.myers@redhat.com>
2014-09-02 18:06:27 -0400

    ProviderTemplate admin view filtering

--- provider_templates/admin.py
+++ provider_templates/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 import models
 
 
+class ProviderTemplateDetailAdmin(admin.ModelAdmin):
+    list_filter = ('tested', 'usable')
+
 admin.site.register(models.Template)
-admin.site.register(models.ProviderTemplateDetail)
+admin.site.register(models.ProviderTemplateDetail, ProviderTemplateDetailAdmin)
 admin.site.register(models.Provider)
 admin.site.register(models.Group)
