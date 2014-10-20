e66474aeb868e51dc438e72b4bbc19eb1f847b66
Sean Myers <sean.myers@redhat.com>
2014-10-20 15:46:53 -0400

    Even more liberal CORS policy

--- middleware.py
+++ middleware.py
@@ -1,6 +1,3 @@
-from django import http
-
-
 class AllowAllXHROrigins(object):
     """Allow all origins and methods for XHR requests
 
@@ -13,18 +10,12 @@ class AllowAllXHROrigins(object):
     """
     METHODS = ','.join(['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'])
 
-    def process_request(self, request):
-        # Intercept preflight requests and respond with all methods/origins
-        if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
-            # This is a reflight request, we can construct the response right here
-            response = http.HttpResponse()
-            response['Access-Control-Allow-Origin'] = '*'
-            response['Access-Control-Allow-Methods'] = self.METHODS
-            return response
-
     def process_response(self, request, response):
         # Add cors headers, rudely overwriting whatever might have been
         # there to allow all origins/methods
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Methods'] = self.METHODS
+        if 'HTTP_ACCESS_CONTROL_REQUEST_HEADERS' in request.META:
+            response['Access-Control-Allow-Headers'] = \
+                request.META['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
         return response
